9e41b8eab6b04c1d7fa38352d13feb347f198d63
spy <zl_zhaolei01@163.com>
2021-02-28 12:23:46 +0800

    数据源配置

--- szse-domain/src/main/java/com/spy/szse/domain/config/MyBatisSzseDbConfig.java
+++ szse-domain/src/main/java/com/spy/szse/domain/config/MyBatisSzseDbConfig.java
@@ -17,26 +17,21 @@ import java.io.IOException;
  * @Author lei.zhao_ext
  * @Date 2021/2/26
  */
-//@Configuration
-/*@MapperScan(basePackages = {MyBatisSzseDbConfig.BASE_PACKAGES},
-        sqlSessionTemplateRef = "sqlSessionTemplate")*/
+@Configuration
+@MapperScan(basePackages = {MyBatisSzseDbConfig.BASE_PACKAGES},
+        sqlSessionTemplateRef = "sqlSessionTemplate")
 public class MyBatisSzseDbConfig {
 
-    /*public static final String BASE_PACKAGES = "com.spy.szse.svc.mapper.szse";
+    public static final String BASE_PACKAGES = "com.spy.szse.svc.mapper.szse";
 
-    @Resource(name = "testDataSource")
-    private DataSource szseDataSource;
-
-    @Bean("szseSqlSessionTemplate")
     public SqlSessionTemplate sqlSessionTemplate(@Qualifier("szseDataSource") DataSource dataSource) throws Exception {
         return new SqlSessionTemplate(sqlSessionFactory(dataSource));
     }
 
     private SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSource);
-        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/mybatis/szse/*.xml"));
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResource("classpath:mapper/mybatis/szse/*.xml"));
         bean.getObject().getConfiguration().setMapUnderscoreToCamelCase(true);
         return bean.getObject();
-    }*/
+    }
 }
