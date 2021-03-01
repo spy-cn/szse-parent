b9c1a2eb6c5b058e021cc5d6853384b5908312ff
lei.zhao_ext <lei.zhao_ext.chinascope.com>
2021-03-01 14:10:44 +0800

    根据关键字查询产品

--- szse-svc/src/main/java/com/spy/szse/svc/service/NodeTableService.java
+++ szse-svc/src/main/java/com/spy/szse/svc/service/NodeTableService.java
@@ -1,7 +1,10 @@
 package com.spy.szse.svc.service;
 
 
+import com.spy.szse.svc.response.ProductKeywordResp;
 import com.spy.szse.svc.response.ProductTreeResp;
+
+import java.util.List;
 
 /**
  * @Author lei.zhao_ext
@@ -10,7 +13,17 @@ import com.spy.szse.svc.response.ProductTreeResp;
 public interface NodeTableService {
     /**
      * 获取产品树
+     *
      * @return
      */
     ProductTreeResp getProductTree();
+
+    /**
+     * 根据关键字查询客户产品
+     *
+     * @param keyword 关键字产品中文名称或者产品编码
+     * @param limit   每页数量 默认20
+     * @return
+     */
+    List<ProductKeywordResp> getProductByKeyword(String keyword, Integer limit);
 }
